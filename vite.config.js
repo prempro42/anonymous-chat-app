1f436ab05e772d08d7ef3e4f8adcc89d08cd59d8
prempro42 <prempro42@gmail.com>
2023-09-23 20:25:48 +0530

    vite config to allow custom env variables

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-  },
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      open: true,
+    },
+    define: {
+      NEXT_PUBLIC_SOCKET_URI: JSON.stringify(env.NEXT_PUBLIC_SOCKET_URI),
+    },
+  };
 });
